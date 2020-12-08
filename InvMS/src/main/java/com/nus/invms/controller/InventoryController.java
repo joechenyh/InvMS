aa00f31c9c7412bc13e477d6e2facc55aa84b7b8
joechenyh <e0533420@u.nus.edu>
2020-12-08 23:19:27 +0800

    replaced inventory controller with usage controller

--- InvMS/src/main/java/com/nus/invms/controller/InventoryController.java
+++
@@ -1,9 +0,0 @@
-package com.nus.invms.controller;
-
-public class InventoryController extends MechanicController {
-
-	public InventoryController() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
---
+++ InvMS/src/main/java/com/nus/invms/controller/UsageController.java
@@ -0,0 +1,9 @@
+package com.nus.invms.controller;
+
+public class UsageController extends MechanicController {
+
+	public UsageController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
