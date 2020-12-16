a0058a741170d44ff5675fb431e8c4d0a6f0376c
Jiayuan-Hu <Jiayuan-Hu-email@example.com>
2020-12-16 19:31:10 +0800

    Update

---
+++ InvMS/src/main/java/com/nus/invms/repo/ProductRepository.java
@@ -0,0 +1,11 @@
+//package com.nus.invms.repo;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import com.nus.invms.domain.Product;
+//
+//public interface ProductRepository extends JpaRepository<Product, Integer> {
+//
+//	
+//
+//}
