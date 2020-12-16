bca767784a103e5b185f37592956bc25b1209b6e
joechenyh <e0533420@u.nus.edu>
2020-12-16 12:38:27 +0800

    commit files to keyin branch

--- InvMS/src/main/java/com/nus/invms/domain/Product.java
+++ InvMS/src/main/java/com/nus/invms/domain/Product.java
@@ -1,19 +1,30 @@
 package com.nus.invms.domain;
+
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+<<<<<<< HEAD
 import javax.validation.constraints.*;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+=======
+import javax.persistence.Table;
+>>>>>>> refs/heads/joe_branch
 
 @Entity
+<<<<<<< HEAD
 @Data
 @NoArgsConstructor
+=======
+@Table(name = "product")
+>>>>>>> refs/heads/joe_branch
 public class Product {
 	
 	@Id
+<<<<<<< HEAD
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int partNumber;
 	@NotNull
@@ -39,6 +50,102 @@ public class Product {
 	} 
 	
 	
+=======
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	int partNumber;
+	
+	int reorderLevel, minReorderQty; 
+	String productName, description, colour, dimension, manufacturer, shelfLocation;
+	Double unitPrice; 
+	
+	
+
+	public Product(int reorderLevel, int minReorderQty, String productName, String description, String colour,
+			String dimension, String manufacturer, String shelfLocation, Double unitPrice) {
+		super();
+		this.reorderLevel = reorderLevel;
+		this.minReorderQty = minReorderQty;
+		this.productName = productName;
+		this.description = description;
+		this.colour = colour;
+		this.dimension = dimension;
+		this.manufacturer = manufacturer;
+		this.shelfLocation = shelfLocation;
+		this.unitPrice = unitPrice;
+	}
+
+	public int getReorderLevel() {
+		return reorderLevel;
+	}
+
+	public void setReorderLevel(int reorderLevel) {
+		this.reorderLevel = reorderLevel;
+	}
+
+	public int getMinReorderQty() {
+		return minReorderQty;
+	}
+
+	public void setMinReorderQty(int minReorderQty) {
+		this.minReorderQty = minReorderQty;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	public String getDimension() {
+		return dimension;
+	}
+
+	public void setDimension(String dimension) {
+		this.dimension = dimension;
+	}
+
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+	public String getShelfLocation() {
+		return shelfLocation;
+	}
+
+	public void setShelfLocation(String shelfLocation) {
+		this.shelfLocation = shelfLocation;
+	}
+
+	public Double getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(Double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+>>>>>>> refs/heads/joe_branch
 
 
 }
