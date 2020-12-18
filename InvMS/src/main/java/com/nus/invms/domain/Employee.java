8a7f687de3437f35b2f8579ceacd839c64e47bfa
iamkeyin <keyintan94@gmail.com>
2020-12-18 16:12:01 +0800

    commit

--- InvMS/src/main/java/com/nus/invms/domain/Employee.java
+++ InvMS/src/main/java/com/nus/invms/domain/Employee.java
@@ -36,7 +36,7 @@ public class Employee{
 	private RoleType role;
 	
 	@OneToMany (mappedBy="employee") 
-	private Collection<Usage> usages;
+	private Collection<PartUsage> partUsages;
 
 	@OneToMany (mappedBy="employee")
 	private Collection<Order> orders;
@@ -50,88 +50,6 @@ public class Employee{
 		this.status = status;
 		this.role = role;
 	}
-
-/*
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	int ID;
-	String username;
-	String epassword;
-	String status;
-	String position;
-	String name;
-
-	
-	
-
-	public Employee(String username, String epassword, String status, String position, String name) {
-		super();
-		this.username = username;
-		this.epassword = epassword;
-		this.status = status;
-		this.position = position;
-		this.name = name;
-	}
-
-*/
-
-	public String getUsername() {
-		return username;
-	}
-
-
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-
-
-	public String getEPassword() {
-		return epassword;
-	}
-
-
-
-	public void setEPassword(String password) {
-		this.epassword = password;
-	}
-
-
-
-	public String getStatus() {
-		return status;
-	}
-
-
-
-	public void setStatus(String status) {
-		this.status = status;
-	}
-
-
-
-	public String getPosition() {
-		return position;
-	}
-
-
-
-	public void setPosition(String position) {
-		this.position = position;
-	}
-
-
-
-	public String getName() {
-		return name;
-	}
-
-
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
 
 
 
