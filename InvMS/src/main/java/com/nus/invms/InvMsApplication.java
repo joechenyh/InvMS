996925b02fe67eaaf5e3ba237d960b9579a85156
joechenyh <e0533420@u.nus.edu>
2020-12-21 20:13:58 +0800

    Revert "Merge remote-tracking branch 'origin/master' into joe_optional_features"
    This reverts commit 7c62b33e77ab7801ee7bc98f0f0be29a31ae9927, reversing
    changes made to 12c9d249bed76d1dd9e2a4af52f4ecc88328933b.

--- InvMS/src/main/java/com/nus/invms/InvMsApplication.java
+++ InvMS/src/main/java/com/nus/invms/InvMsApplication.java
@@ -37,7 +37,7 @@ import com.nus.invms.repo.SupplierRepository;
 @SpringBootApplication
 public class InvMsApplication {
 	
-	
+	//testtt
 	
 	@Autowired
 	EmployeeRepository erepo; 
@@ -67,12 +67,10 @@ public class InvMsApplication {
 	
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-
-
-		return args -> 
-		{
-
-			Employee e1 = new Employee("John Doe", "qwerty", "qwerty", Status.ACTIVE, RoleType.ADMIN); 
+		return args -> {
+			
+			
+			Employee e1 = new Employee("Joe Chen", "joechen", "password", Status.ACTIVE, RoleType.ADMIN); 
 			Employee e2 = new Employee("Mechanic", "mechanic", "password", Status.ACTIVE, RoleType.MECHANIC); 
 			Employee e3 = new Employee("Admin", "admin", "password", Status.ACTIVE, RoleType.ADMIN); 
 			erepo.save(e1);
@@ -82,13 +80,10 @@ public class InvMsApplication {
 			Supplier s1 = new Supplier("Spare Part Supplier Company", Status.ACTIVE, "91276332", "1 Heng Mui Keng Terrace", "sparepart@gmail.com");
 			srepo.save(s1);
 			
-			Product p1 = new Product(1001, 10, 20, "Sample Product Name", "Sample Description", "Red", "Sample Dimension", "Sample Manufacturer", "Shelf 1A", 23.00, Status.ACTIVE);
+			Product p1 = new Product(1, 10, 20, "Sample Product Name", "Sample Description", "Red", "Sample Dimension", "Sample Manufacturer", "Shelf 1A", 23.00, Status.ACTIVE);
 			prepo.save(p1);
-			Product p2 = new Product(1002, 11, 22, "Sample Product Name", "Sample Description", "Red", "Sample Dimension", "Sample Manufacturer", "Shelf 1A", 25.00, Status.ACTIVE);
-			prepo.save(p2);
 			
-
-			Inventory i21 = new Inventory(1, "Random Brand", "This stock is on special offer", "Car Windows", "Front Windows", "Unbreakable Windows", 23.00, 35.00, 47.00, 96.00, "Anti Skid Tyre with ultra hard rubber", 35, p1, s1, s1.getSupplierName());
+			Inventory i1 = new Inventory(1, "Random Brand", "This stock is on special offer", "Car Windows", "Front Windows", "Unbreakable Windows", 23.00, 35.00, 47.00, 96.00, "Anti Skid Tyre with ultra hard rubber", 35, p1, s1, s1.getSupplierName());
 			Inventory i2 = new Inventory(2, "King Brand", "This stock is on special offer", "Car Windows", "Front Windows", "Unbreakable Windows", 23.00, 35.00, 47.00, 96.00, "Anti Skid Tyre with ultra hard rubber", 35, p1, s1, s1.getSupplierName());
 			Inventory i3 = new Inventory(3, "Singa Brand", "This stock is on special offer", "Car Windows", "Front Windows", "Unbreakable Windows", 23.00, 35.00, 47.00, 96.00, "Anti Skid Tyre with ultra hard rubber", 35, p1, s1, s1.getSupplierName());
 			Inventory i4 = new Inventory(4, "Coco Brand", "This stock is on special offer", "Car Windows", "Front Windows", "Unbreakable Windows", 23.00, 35.00, 47.00, 96.00, "Anti Skid Tyre with ultra hard rubber", 35, p1, s1, s1.getSupplierName());
@@ -108,11 +103,7 @@ public class InvMsApplication {
 			Inventory i18 = new Inventory(8, "Rocky Brand", "This stock is expiring soon", "Car Parts", "Tyres", "Tyres for Rainy Days", 23.00, 35.00, 47.00, 96.00, "Anti Skid Tyre with ultra hard rubber", 35, p1, s1, s1.getSupplierName());
 			Inventory i19 = new Inventory(9, "Metal Brand", "This stock is expiring soon", "Car Parts", "Tyres", "Tyres for Rainy Days", 23.00, 35.00, 47.00, 96.00, "Anti Skid Tyre with ultra hard rubber", 35, p1, s1, s1.getSupplierName());
 			Inventory i20 = new Inventory(10, "Caterpillar Brand", "This stock is expiring soon", "Car Parts", "Tyres", "Tyres for Rainy Days", 23.00, 35.00, 47.00, 96.00, "Anti Skid Tyre with ultra hard rubber", 35, p1, s1, s1.getSupplierName());
-
-			Inventory i1 = new Inventory(1, "Nike", "lala", "Big", "There", "here", 12.00, 13.00, 14.00, 15.00, "Sample Product Name", 100, p1, s1, "Spare Part Supplier Company");
-
 			irepo.save(i1);
-
 			irepo.save(i2);
 			irepo.save(i3);
 			irepo.save(i4);
@@ -132,9 +123,10 @@ public class InvMsApplication {
 			irepo.save(i18);
 			irepo.save(i19);
 			irepo.save(i20);
-			irepo.save(i21);
-
-
+			
+			
+			/*	Order o1 = new Order();*/
+				
 			PartUsage pu1 = new PartUsage(e1, p1.getPartNumber(), 2, LocalDate.now(), "SDV1234R");
 				
 			purepo.save(pu1);
@@ -142,7 +134,6 @@ public class InvMsApplication {
 			System.out.println("Team Alpha Bean - Ready to Run!");
 	
 
-
-	} ;
-}
+};
+	} 
 }
